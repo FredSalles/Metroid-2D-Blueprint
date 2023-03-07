b1f7fb7a07c9d50d4b4aa00aeb92423316ed4a72
FredSalles <fredsalles@me.com>
2023-03-07 11:54:42 -0800

    first commit

---
+++ src/allMap.ts
@@ -0,0 +1,82 @@
+let map = {
+    "name": "Brinstar",
+    "width": 32,
+    "height": 20,
+    "orientation": "horizontal",
+    "data": [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ]
+};
+
+let canvasWitdh = 512;
+let canvasHeight = 480;
+let canvasBlockWitdh = 16;
+let canvasBlockHeight = 15;
+let blockWitdh = 32;
+let blockHeight = 32;
+let cameraX = 0;
+let cameraY = 0;
+let step = 16;
+let ctx, img0, img1, img2, images;
+
+function renderCanvasTotal() {
+    //console.log('renderCanvasTotal...');
+    ctx.strokeStyle = "blue";
+    for (let i = 0; i < map.height; i++) {
+        for (let j = 0; j < map.width; j++) {
+            ctx.drawImage(
+                images[map.data[i][j]],
+                j * blockWitdh,
+                i * blockHeight);
+            //ctx.strokeRect(j * blockWitdh, i * blockHeight, blockWitdh,blockHeight);
+        }
+    }
+}
+
+//
+// On Key Down
+//
+window.onkeydown = function (event) {
+    //console.log('window.onkeydown[' + event.key + ']...');
+    if (event.key === "ArrowRight") {
+        //move origin(0,0) to the left
+        ctx.translate(-step,0);
+        renderCanvasTotal();
+    }
+    if (event.key === "ArrowLeft") {
+        ctx.translate(step,0);
+        renderCanvasTotal();
+    }
+}
+
+//
+// On Window Load
+//
+window.onload = () => {
+    console.log('Window.onLoad...');
+    ctx = document.getElementById("canvas").getContext("2d");
+    img0 = document.getElementById("block_0");
+    img1 = document.getElementById("block_1");
+    img2 = document.getElementById("block_2");
+    images = [img0, img1, img2];
+    renderCanvasTotal();
+}
---
+++ src/cameraOnly.ts
@@ -0,0 +1,102 @@
+let map = {
+    "name": "Brinstar",
+    "width": 32,
+    "height": 20,
+    "orientation": "horizontal",
+    "data": [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ]
+};
+
+let canvasWitdh = 512;
+let canvasHeight = 480;
+let canvasBlockWitdh = 16;
+let canvasBlockHeight = 15;
+let blockWitdh = 32;
+let blockHeight = 32;
+let cameraX = 0;
+let cameraY = 0;
+let step = 16;
+let ctx, img0, img1, img2, images;
+
+function renderCanvasOptimized() {
+    //console.log('renderCanvasOptimized...');
+
+    let firstBlockX = Math.floor(cameraX / blockWitdh);
+    let firstBlockY = Math.floor(cameraY / blockHeight);
+    let drawPosOffsetX = cameraX % blockWitdh;
+    let drawPosOffsetY = cameraY % blockHeight;
+
+    for (let i = firstBlockY; i < firstBlockY + canvasBlockHeight + 1; i++) {
+        for (let j = firstBlockX; j < firstBlockX + canvasBlockWitdh + 1; j++) {
+            if ((j < map.width) && (i < map.height)) {
+                let x = ((j - firstBlockX) * blockWitdh) - drawPosOffsetX;
+                let y = ((i - firstBlockY) * blockHeight) - drawPosOffsetY;
+                ctx.drawImage(images[map.data[i][j]], x, y);
+            }
+        }
+    }
+}
+
+//
+// On Key Down
+//
+window.onkeydown = function (event) {
+    //console.log('window.onkeydown[' + event.key + ']...');
+    if (event.key === "ArrowRight") {
+        if (cameraX < ((map.width) * blockWitdh) - (canvasBlockWitdh * blockWitdh)) {
+            cameraX += step;
+            renderCanvasOptimized();
+        }
+    }
+    if (event.key === "ArrowLeft") {
+        if (cameraX >= step) {
+            cameraX -= step;
+            renderCanvasOptimized();
+        }
+    }
+    if (event.key === "ArrowUp") {
+        if (cameraY >= step) {
+            cameraY -= step;
+            renderCanvasOptimized();
+        }
+    }
+    if (event.key === "ArrowDown") {
+        if (cameraY < ((map.height) * blockHeight) - (canvasBlockHeight * blockHeight)) {
+        cameraY += step;
+        renderCanvasOptimized();
+        }
+    }
+}
+
+//
+// On Window Load
+//
+window.onload = () => {
+    console.log('Window.onLoad...');
+    ctx = document.getElementById("canvas").getContext("2d");
+    img0 = document.getElementById("block_0");
+    img1 = document.getElementById("block_1");
+    img2 = document.getElementById("block_2");
+    images = [img0, img1, img2];
+    renderCanvasOptimized();
+}
