39dc207d349a0214d449d309cd90f4b75b5fa12c
FredSalles <fredsalles@me.com>
2023-03-08 10:34:27 -0800

    some cleanup

---
+++ src/AllMapOffCanvas/allMapOffCanvas.ts
@@ -0,0 +1,137 @@
+let map = {
+    "name": "Brinstar",
+    "width": 32,
+    "height": 20,
+    "orientation": "horizontal",
+    "backdrop" : [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 0],
+        [0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0],
+        [0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ],
+    "data": [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ]
+};
+
+let canvasWitdh = 512;
+let canvasHeight = 480;
+let canvasBlockWitdh = 16;
+let canvasBlockHeight = 15;
+let blockWitdh = 32;
+let blockHeight = 32;
+let cameraX = 0;
+let cameraY = 0;
+let step = 16;
+let ctx, img0, img1, img2, img3, images, offcanvas, offctx, backcanvas, backctx;
+let posX = 0;
+let posY = 0;
+
+function allMapOffCanvasRender(c,h,w,d,t) {
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            if ((d[i][j] == 0) && t) {
+                continue;
+            }
+            c.drawImage(images[d[i][j]], j * blockWitdh, i * blockHeight);
+        }
+    }
+}
+
+//
+// On Key Down
+//
+window.onkeydown = function (event) {
+    if (event.key === "ArrowRight") {
+        if (posX <((map.width) * blockWitdh) - (canvasBlockWitdh * blockWitdh)) {
+            ctx.setTransform(1,0,0,1,0,0);
+            ctx.drawImage(backcanvas ,0 ,0);
+            ctx.translate(-step,0);
+        ctx.drawImage(offcanvas ,0 ,0);
+        posX += step;
+    }
+    }
+    if (event.key === "ArrowLeft") {
+        if (posX > 0) {
+            ctx.setTransform(1,0,0,1,0,0);
+            ctx.drawImage(backcanvas ,0 ,0);
+            ctx.translate(step,0);
+            ctx.drawImage(offcanvas ,0 ,0);
+            posX -= step;
+        }
+    }
+    if (event.key === "ArrowUp") {
+        if (posY > 0) {
+            ctx.setTransform(1,0,0,1,0,0);
+            ctx.drawImage(backcanvas ,0 ,0);
+            ctx.translate(0,step);
+        ctx.drawImage(offcanvas ,0 ,0);
+        posY -= step;
+        }
+    }
+    if (event.key === "ArrowDown") {
+        if ( posY <((map.height) * blockHeight) - (canvasBlockHeight * blockHeight)) {
+            ctx.setTransform(1,0,0,1,0,0);
+            ctx.drawImage(backcanvas ,0 ,0);
+            ctx.translate(0,-step);
+        ctx.drawImage(offcanvas ,0 ,0);
+        posY += step;
+    }
+    }
+}
+
+//
+// On Window Load
+//
+window.onload = () => {
+    console.log('Window.onLoad...');
+    ctx = document.getElementById("canvas").getContext("2d");
+    img0 = document.getElementById("block_0");
+    img1 = document.getElementById("block_1");
+    img2 = document.getElementById("block_2");
+    img3 = document.getElementById("block_3");
+    images = [img0, img1, img2, img3];
+    offcanvas = document.createElement('canvas');
+    offcanvas.width = map.width * blockWitdh;
+    offcanvas.height = map.height * blockHeight;
+    backcanvas = document.createElement('canvas');
+    backcanvas.width = canvasWitdh;
+    backcanvas.height = canvasHeight;
+    offctx = offcanvas.getContext("2d");
+    backctx = backcanvas.getContext("2d");
+    allMapOffCanvasRender(offctx, map.height, map.width, map.data, true);
+    allMapOffCanvasRender(backctx, canvasBlockHeight, canvasBlockWitdh, map.backdrop, false);
+
+    ctx.drawImage(backcanvas ,0 ,0);
+    ctx.drawImage(offcanvas ,0 ,0);
+}
